d8d8bc58c760b9d4afc4fb61530499762b9dcd9c
r3czus <76210775+r3czus@users.noreply.github.com>
2022-09-12 15:46:26 +0200

    .
    .

---
+++ aplikacjaKonsolowaEgazmin/ConsoleApp1/ConsoleApp1/Osoba.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Osoba
+    {
+        private int id { get; set; }
+        private string imie { get; set; }
+        public static int ileInstancji = 0;
+
+        
+        public Osoba()
+        {
+            this.id = 0;
+            this.imie ="";
+            ileInstancji++;
+        }
+        public Osoba(int id, string imie)
+        {
+            this.id = id;
+            this.imie = imie;
+            ileInstancji++;
+        }
+        public Osoba(Osoba osoba)
+        {
+            this.id=osoba.id;
+            this.imie = osoba.imie;
+            ileInstancji++;
+        }
+        public void PodajImie(String imie)
+        {
+            if(this.imie == "")
+            {
+                Console.WriteLine("brak danych");
+            }
+            else
+            Console.WriteLine("Czejsć"+imie+",mam na imie " +this.imie);
+        }
+    }
+   
+}
---
+++ aplikacjaKonsolowaEgazmin/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba= new Osoba(1,"kinga");
+            osoba.PodajImie("kasia");
+            Console.ReadLine();
+            Osoba osoba2 = new Osoba();
+            osoba2.PodajImie("Maga");
+            Console.ReadLine();
+        }
+    }
+}
